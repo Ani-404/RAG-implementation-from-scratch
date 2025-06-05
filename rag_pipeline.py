9507e513a14b472f53352541c28504bbcf644865
Animesh <animesh27k@gmail.com>
2025-06-05 18:10:45 +0530

    updated code

--- rag_pipeline.py
+++ rag_pipeline.py
@@ -59,4 +59,46 @@ class RAGPipeline:
         self.bm25           = None
 
 
+    def chunk_text(self,
+                   text: str,
+                   doc_name: str,
+                   doc_id: str,
+                   method: str = "recursive",
+                   chunk_size: int = 500,
+                   overlap: int = 50,
+                   markdown_headers: Optional[List[Dict[str, str]]] = None) -> List[Dict[str, str]]:
+        """
+        Splits text into smaller chunks and adds metadata to each chunk.
+
+        Args:
+            text (str): Document content.
+            doc_name (str): Name of the document.
+            doc_id (str): Unique document identifier.
+            method (str): Chunking strategy: "fixed", "recursive", or "document".
+            chunk_size (int): Size of chunks.
+            overlap (int): Overlapping characters between chunks.
+            markdown_headers (Optional[List[Dict[str, str]]]): Headers for Markdown/HTML chunking.
+
+        Returns:
+            List[Dict[str, str]]: List of chunks with metadata.
+        """
+        if method == 'fixed':
+            splitter = CharacterTextSplitter(chunk_size=chunk_size, chunk_overlap=overlap)
+        elif method == 'recursive':
+            splitter = RecursiveCharacterTextSplitter(chunk_size=chunk_size, chunk_overlap= overlap)
+        elif method == 'document':
+            if markdown_headers:
+                splitter = MarkdownHeaderTextSplitter(headers_to_split_on=markdown_headers)
+            else:
+                CharacterTextSplitter(chunk_size=chunk_size, chunk_overlap=overlap)
+        else:
+            raise ValueError("Unsupported chunking method. Choose from 'fixed', 'recursive', and 'document'.")
+
+        chunks = splitter.split_text(text)
+        return [{"id": f"{doc_id}_{i}", "text": chunk} for i, chunk in enumerate(chunks)]
+    
+
+    
+
+
 
